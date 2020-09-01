76717c79448685c2054eb26a77e82b9ef8db83b0
Jason Zheng <one@fish.cat>
2020-09-01 19:51:23 -0400

    Setting up Express and mongodb

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/scores', (req, res, next) => {
+
+  // Implement
+
+});
+
+router.post('/scores', (req, res, next) => {
+
+  // Implement
+
+});
+
+router.delete('/scores/:id', (req, res, next) => {
+
+  // Implement
+
+});
